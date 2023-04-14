9a676ac5592e960d8ff16df6820a9ed4b621895b
Thomas Völkel <thomas.voelkel@hdnet.de>
2023-04-14 15:01:31 +0200

    style(types): Format types to have one in each line

--- index.js
+++ index.js
@@ -28,7 +28,21 @@ module.exports = {
         'type-enum': [
             2,
             'always',
-            ['build', 'chore', 'ci', 'docs', 'dx', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'types'],
+            [
+                'build',
+                'chore',
+                'ci',
+                'docs',
+                'dx',
+                'feat',
+                'fix',
+                'perf',
+                'refactor',
+                'revert',
+                'style',
+                'test',
+                'types',
+            ],
         ],
     },
 }
